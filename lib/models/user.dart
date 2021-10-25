c0f3d7fa89623a065663a46112114f75dc2f6ed5
msyamsularif <syamsularif0904@gmail.com>
2021-10-25 10:55:07 +0700

    Update model user

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -32,6 +32,28 @@ class User extends Equatable {
         city,
         picturePath,
       ];
+
+  User copyWith({
+    int? id,
+    String? name,
+    String? email,
+    String? address,
+    String? houseNumber,
+    String? phoneNumber,
+    String? city,
+    String? picturePath,
+  }) {
+    return User(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      email: email ?? this.email,
+      address: address ?? this.address,
+      houseNumber: houseNumber ?? this.houseNumber,
+      phoneNumber: phoneNumber ?? this.phoneNumber,
+      city: city ?? this.city,
+      picturePath: picturePath ?? this.picturePath,
+    );
+  }
 }
 
 User mockUser = User(
